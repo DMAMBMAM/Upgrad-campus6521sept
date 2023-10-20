d9f0817b55ae8b557650f32cb1ffdff079c8f4df
DMAMBMAM <debajyotimahata21@gmail.com>
2023-10-20 17:57:09 +0530

    https://github.com/DMAMBMAM/Upgrad-campus6521sept.git

---
+++ src/pattern_2.java
@@ -0,0 +1,10 @@
+public class pattern_2 {
+    public static void main(String[] args){
+        int i,j;
+        int n=5;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i+1;j++)
+            System.out.println(j);}
+        System.out.println("\n");
+    }
+}
